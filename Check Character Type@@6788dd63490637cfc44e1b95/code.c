269b9c7c841c21cb37e02c0e368a3c17c5a5133b
Vans30-m <vansh0237.becse24@chitkara.edu.in>
2025-01-28 11:11:58 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -15,7 +15,7 @@ int main() {
             printf("Consonant");
         }
     }   
-    else if (a=='0' || a=='1' || a=='2' || a=='3' || a=='4' || a=='5' || a=='6' || a=='7' || a=='8' || a=='9'){
+    else if (a==0 || a==1 || a==2 || a==3 || a==4 || a==5 || a==9 || a==6 || a==7 || a==8){
         printf("Digit");
     }
     else{

fc7f6ce617120776a2832775ac84012d005ea280
Vans30-m <vansh0237.becse24@chitkara.edu.in>
2025-01-29 10:38:47 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+int main() {
+    int a,b;
+    char op;
+    scanf("%a %b",&a,&b);
+    scanf("%c",&op);
+
+    if (c=='+'){
+        printf("%d",a+b);
+    }
+    else{
+        printf("Error");
+    }
+    return 0;
+}

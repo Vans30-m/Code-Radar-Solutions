4f836ddc36784486cb4cb0085dde26b250120d68
Vans30-m <vansh0237.becse24@chitkara.edu.in>
2025-02-02 01:06:59 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -9,20 +9,29 @@ int main() {
     scanf("%d",&b);
     char str;
     scanf("%c",&str);
-    if(str='+'){
-        printf("%d",a+b);
-    }
-    else if(str='-'){
-        printf("%d",a-b);
-    }
-    else if(str=='/'){
-        printf("%d",a/b);
-    }
-    else if(str=='*'){
-        printf("%d",a*b);
-    }
-    else{
-        printf("error");
-    }
+    switch(str){
+        case'+'{
+            printf("%d",a+b);
+            break;
+        }
+        case'-'{
+            printf("%d",a-b);
+            break;
+        }
+    // }(str='+'){
+    //     printf("%d",a+b);
+    // }
+    // else if(str='-'){
+    //     printf("%d",a-b);
+    // }
+    // else if(str=='/'){
+    //     printf("%d",a/b);
+    // }
+    // else if(str=='*'){
+    //     printf("%d",a*b);
+    // }
+    // else{
+    //     printf("error");
+    // }
     return 0;
 }

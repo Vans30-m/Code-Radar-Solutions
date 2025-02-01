7fc021d7f2fa2792423e266c93c04b42883ab943
Vans30-m <vansh0237.becse24@chitkara.edu.in>
2025-02-02 01:04:00 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,10 +3,11 @@
 
 
 int main() {
-    char str;
+    // char str;
     int a,b;
     scanf("%d",&a);
     scanf("%d",&b);
+    char str;
     scanf("%c",&str);
     if(str=='+'){
         printf("%d",a+b);

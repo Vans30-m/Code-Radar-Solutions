09673fd720cc6f401436bfe0c1884956b875acd3
Vans30-m <vansh0237.becse24@chitkara.edu.in>
2025-02-02 00:32:01 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -5,14 +5,14 @@ int main() {
 
     for (int x;x<=N;x++){
         char book[100],author[100];
-        scanf("%c %c",&book,&author);
+        scanf("%s %s",&book,&author);
         int price;
         scanf("%d",&price);
         int threshold;
         scanf("%d",&threshold);
         if (price>threshold){
             printf("Books above price %0.2f:\n",threshold);
-            printf("Title: %s, Author: %s, Price:%0.2f\n",book,author,price);
+            printf("Title: %c, Author: %c, Price:%0.2f\n",book,author,price);
         }
         else{
             printf("HI");

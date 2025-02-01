6b37920451b2c83bbfedab93a7c4298e76a7ed70
Vans30-m <vansh0237.becse24@chitkara.edu.in>
2025-02-02 00:37:14 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -12,19 +12,13 @@ int main() {
         scanf("%0.2f",&price);
         int threshold;
         scanf("%d",&threshold);
-        // if (price>threshold){
-        //     printf("Books above price %0.2f:\n",threshold);
-        //     printf("Title: %s, Author: %s, Price:%0.2f\n",book,author,price);
-        // }
-        // else{
-        //     printf("HI");
-        // }
-    }
-    if(price>threshold){
-        printf("hi");
-    }
-    else{
-        printf("ji");
+        if (price>threshold){
+            printf("Books above price %0.2f:\n",threshold);
+            printf("Title: %s, Author: %s, Price:%0.2f\n",book,author,price);
+        }
+        else{
+            printf("HI");
+        }
     }
     return 0;
 }

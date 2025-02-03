3240b5c2ea7d59a7c2e3a14b3a18d0604b237b58
Vans30-m <vansh0237.becse24@chitkara.edu.in>
2025-02-03 15:30:05 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,35 +3,30 @@
 
 
 int main() {
-    // char str;
-    int a,b;
-    scanf("%d",&a);
-    scanf("%d",&b);
+    double a,b;
+    scanf("%lf",&a);
+    scanf("%lf",&b);
     char str;
     scanf("%c",&str);
-    switch(str){
-        case'+':
-            printf("%d",a+b);
-            break;
-        
-        case'-':
-            printf("%d",a-b);
-            break;
-        
-    // }(str='+'){
-    //     printf("%d",a+b);
-    // }
-    // else if(str='-'){
-    //     printf("%d",a-b);
-    // }
-    // else if(str=='/'){
-    //     printf("%d",a/b);
-    // }
-    // else if(str=='*'){
-    //     printf("%d",a*b);
-    // }
-    // else{
-    //     printf("error");
+    if (str='+'){
+        printf("%d",a+b);
+    }
+    else if(str='-'){
+        printf("%d",a-b);
+    }
+    else if(str=='/'){
+        if (b==0){
+            printf("error");
+        }
+        else{
+        printf("%d",a/b);
+        }
+    }
+    else if(str=='*'){
+        printf("%d",a*b);
+    }
+    else{
+        printf("error");
     }
     return 0;
 }

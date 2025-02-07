958bba996095e552159f01554d22bbfeb0da9307
Vans30-m <vansh0237.becse24@chitkara.edu.in>
2025-02-07 21:08:16 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -8,6 +8,12 @@ else {
 }
 
 int main() {
-
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int num;
+        scanf("%d",&num);
+        printf("%d\n",isprime(num));
+    }
     return 0;
 }

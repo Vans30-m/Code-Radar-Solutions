f503da5be9739049cf9371cb0dc5d155b0ba0f58
Vans30-m <vansh0237.becse24@chitkara.edu.in>
2025-03-08 17:47:08 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -10,7 +10,7 @@ int main(){
     int found=0;
     for(int i=0;i<n;i++){
         int remainder=arr[i]%10;
-        int reversed[i]=(reversed*10)+remainder;
+        int reversed=(reversed*10)+remainder;
         arr[i] /= 10;
         found=1;
         if(arr[i]==reversed){
